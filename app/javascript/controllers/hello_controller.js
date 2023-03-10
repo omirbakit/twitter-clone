af98c80d477cf518b4cbcd34f730084132fd41f8
omirbakit <omersat@mail.ru>
2023-03-10 16:50:00 +0500

    Add Tweet form controller

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="tweet-form"
 export default class extends Controller {
   connect() {
     this.element.textContent = "Hello World!"
