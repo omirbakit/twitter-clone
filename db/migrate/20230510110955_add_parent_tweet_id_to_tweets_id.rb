188824e5b45eefac143b88548af1a00d790579f9
omirbakit <omersat@mail.ru>
2023-05-10 17:07:59 +0500

    Modeling reply tweets part27

---
+++ db/migrate/20230510110955_add_parent_tweet_id_to_tweets_id.rb
@@ -0,0 +1,6 @@
+class AddParentTweetIdToTweetsId < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tweets, :parent_tweet_id, :bigint, index: true
+    add_foreign_key :tweets, :tweets, column: :parent_tweet_id
+  end
+end
