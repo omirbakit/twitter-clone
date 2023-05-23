79f89a391ecb81eacd52f2d3763f3ccb1fd994af
omirbakit <omersat@mail.ru>
2023-05-23 11:36:21 +0500

    Settin up Followins part34

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   it { should have_many(:tweets).dependent(:destroy) }
-  it { should validate_uniqueness_of(:username).case_insensitive.allow_blank }
   it { should have_many(:likes).dependent(:destroy) }
   it { should have_many(:liked_tweets).through(:likes).source(:tweet) }
   it { should have_many(:bookmarks).dependent(:destroy) }
@@ -11,6 +10,12 @@ RSpec.describe User, type: :model do
   it { should have_many(:retweeted_tweets).through(:retweets).source(:tweet) }
   it { should have_many(:views) }
   it { should have_many(:viewed_tweets).through(:views).source(:tweet) }
+  it { should have_many(:followings).dependent(:destroy) }
+  it { should have_many(:following_users).through(:followings) }
+  it { should have_many(:reverse_followings).with_foreign_key(:following_user_id).class_name("Following") }
+  it { should have_many(:followers).through(:reverse_followings).source(:user) }
+
+  it { should validate_uniqueness_of(:username).case_insensitive.allow_blank }
 
   describe "#set_display_name" do
     context "when display_name is set" do
