53ef92ac073f9452389b7175e249f66007feeb66
omirbakit <omersat@mail.ru>
2023-06-09 15:01:10 +0500

    Rendering Tweets om Users page part39

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
   def show
     redirect_to profile_path if params[:id].to_i == current_user.id
     @user = User.find(params[:id])
+    @tweet_presenters = @user.tweets.map do |tweet|
+      TweetPresenter.new(tweet: tweet, current_user: @user)
+    end
   end
 end
