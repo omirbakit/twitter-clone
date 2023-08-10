d3220fd23df1826f36b5978308c6b59eb66a1949
omirbakit <omersat@mail.ru>
2023-08-10 17:02:54 +0500

    Rendering Message Threads part56

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -2,8 +2,10 @@ class MessageThreadsController < ApplicationController
   before_action :authenticate_user!
 	
 	def index
-		if params[:user_id].present?
-			@new_message_thread_user = User.find(params[:user_id])
-		end
+    message_thread_ids = MessageThreadsUser.where(user: current_user).pluck(:message_thread_id)
+    @message_threads = MessageThread.includes(:users, :messages).where(id: message_thread_ids)
+    if params[:user_id].present?
+      @new_message_thread_user = User.find(params[:user_id])
+    end
 	end
 end
