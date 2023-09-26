5ed742f94dfeea280fd10d4d541558ca6567d676
omirbakit <omersat@mail.ru>
2023-09-26 10:39:57 +0500

    Paginating through Tweets in Dashboard part81

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tweet_activities = current_user.tweet_activities.order(created_at: :desc).page(1).per(5)
+    @tweet_activities = current_user.tweet_activities.order(created_at: :desc).page(params[:page]).per(10)
     @tweet_activities_data = {
       tweet_activities: @tweet_activities.map do |tweet_activity|
         TweetPresenter.new(tweet: tweet_activity.tweet, current_user: current_user, tweet_activity: tweet_activity)
@@ -12,5 +12,10 @@ class DashboardController < ApplicationController
       last_page: @tweet_activities.last_page?,
       total_pages: @tweet_activities.total_pages
     }
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 end
