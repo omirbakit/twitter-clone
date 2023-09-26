02637fb0f1358c7f5ae35ed3ebb00572267fb9dc
omirbakit <omersat@mail.ru>
2023-09-26 12:09:55 +0500

    Fixing infinite scrolling bug part82

--- app/javascript/controllers/dashboard_tweets_controller.js
+++ app/javascript/controllers/dashboard_tweets_controller.js
@@ -9,9 +9,11 @@ export default class extends Controller {
     window.addEventListener('scroll', () => {
       if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
         if (this.element.dataset.lastPage === 'false') {
-          fetch(`/?page=${this.element.dataset.nextPage}`, { headers: this.headers })
+          fetch(`/dashboard?page=${this.element.dataset.nextPage}`, { headers: this.headers })
             .then(response => response.text())
-            .then(html => Turbo.renderStreamMessage(html))
+            .then(html => Turbo.renderStreamMessage(html));
+
+          this.disconnect();
         }
       }
     });
